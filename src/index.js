6700297dfd0ef987a6621f65fa8c811e89d23ee1
Giannoulo <ggiannoulopoulos@konnektable.com>
2021-09-13 12:29:32 +0300

    Trying service workers

--- src/index.js
+++ src/index.js
@@ -18,4 +18,15 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register("/sw.js");
+// registerSW();
+
+// async function registerSW() {
+//   if ("serviceWorker" in navigator) {
+//     try {
+//       await navigator.serviceWorker.register("/sw.js");
+//     } catch (e) {
+//       console.log(`SW registration failed`);
+//     }
+//   }
+// }
