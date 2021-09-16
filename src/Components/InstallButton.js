8b287758fd7c5d1e76d323f5df3263d26d1e8040
Giannoulo <giannoulopoulosg@gmail.com>
2021-09-16 14:56:59 +0300

    Changed install text

--- src/Components/InstallButton.js
+++ src/Components/InstallButton.js
@@ -5,7 +5,7 @@ import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
 const InstallButton = () => {
   return (
     <div id="install-button-constainer">
-      <FontAwesomeIcon icon={faMobileAlt} /> Install
+      <FontAwesomeIcon icon={faMobileAlt} /> Install App
     </div>
   );
 };
