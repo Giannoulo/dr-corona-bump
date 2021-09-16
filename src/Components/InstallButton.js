37be0a17684973e326dcbf62bb103be5717c9bbf
Giannoulo <giannoulopoulosg@gmail.com>
2021-09-16 15:27:21 +0300

    Install button testing

--- src/Components/InstallButton.js
+++ src/Components/InstallButton.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
 
 const InstallButton = () => {
+  useEffect(() => {
+    const handle_storePrompt = (e) => {
+      e.preventDefault();
+      alert("Action Triggered");
+    };
+    window.addEventListener("beforeinstallprompt", (e) => handle_storePrompt(e));
+    return (_) => {
+      window.removeEventListener("beforeinstallprompt", (e) => handle_storePrompt(e));
+    };
+  }, []);
+
   return (
     <div id="install-button-constainer">
       <FontAwesomeIcon icon={faMobileAlt} /> Install App
