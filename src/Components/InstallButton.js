a62e9d0c1dd10113b709e4f1a7e4b19cb12df5c1
Giannoulo <giannoulopoulosg@gmail.com>
2021-09-16 16:02:26 +0300

    Adding install app functionality

--- src/Components/InstallButton.js
+++ src/Components/InstallButton.js
@@ -1,21 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
 
 const InstallButton = () => {
+  const [visibleInstallButton, setVisibleInstallButton] = useState(false);
+  const [installPrompt, setInstallPrompt] = useState(null);
+
   useEffect(() => {
-    const handle_storePrompt = (e) => {
+    const handleInstallPrompt = (e) => {
       e.preventDefault();
-      alert("Action Triggered");
+      setVisibleInstallButton(true);
+      setInstallPrompt(e);
     };
-    window.addEventListener("beforeinstallprompt", (e) => handle_storePrompt(e));
+    window.addEventListener("beforeinstallprompt", (e) => handleInstallPrompt(e));
     return (_) => {
-      window.removeEventListener("beforeinstallprompt", (e) => handle_storePrompt(e));
+      window.removeEventListener("beforeinstallprompt", (e) => handleInstallPrompt(e));
     };
   }, []);
 
   return (
-    <div id="install-button-constainer">
+    <div
+      id="install-button-constainer"
+      className={visibleInstallButton ? "visible" : ""}
+      onClick={installPrompt.prompt}
+    >
       <FontAwesomeIcon icon={faMobileAlt} /> Install App
     </div>
   );
